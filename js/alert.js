95f22bfa4cea7c1d4a91163d7413203f7b528bc8
Monica Paultre <paulmj1@mail.broward.edu>
2024-11-05 17:30:48 -0500

    JAVASCRIPT FIX

---
+++ js/alert.js
@@ -0,0 +1,7 @@
+function popUp() {
+var input = prompt("How did you hear about us?")
+
+var response = "That's awesome! I love " + input + "! Thank you so much, I hope you have an amazing day!!";
+
+alert(response);
+}
